8919644dbc9e0e03c150b629deebaf006cd630e5
Matheus Arthur <matheus.artsilva@gmail.com>
2026-02-27 18:22:29 -0300

    learn: encerra codigo 2.5.4
    - vimos o grid system do bootstrap
    - a solucao esta adaptada para lidar com quatro pedidos
    - o mecanismo de responsividade foi atualizado

---
+++ ap04-props/src/old_App.jsx
@@ -0,0 +1,26 @@
+const App = () => (
+  <div className="container border py-5">
+    <div className="row">
+      <div className="col-sm-12 col-md-3 col-lg-3 p-4 bg-primary">
+        Caixa vermelha
+      </div>
+      <div className="col-sm-12 col-md-9 col-lg-9 p-4 bg-secondary">
+        Caixa azul
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-sm-12 col-md-6 col-lg-4 p-4 bg-primary"></div>
+      <div className="col-sm-12 col-md-6 col-lg-4 p-4 bg-secondary"></div>
+      <div className="col-sm-12 col-md-12 col-lg-4 p-4 bg-primary"></div>
+
+    <div className="row">
+      <col-sm-12 className="col-md-6 col-lg-4 bg-info p-4"></col-sm-12>
+      <col-sm-12 className="col-md-6 col-lg-4 bg-info p-4"></col-sm-12>
+      <col-sm-12 className="col-md-6 col-lg-4 bg-info p-4"></col-sm-12>
+    </div>
+    </div>
+  </div>
+)
+
+export default App
