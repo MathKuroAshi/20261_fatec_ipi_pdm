221f98e115f1b8fe09be3d2bc29c9dd1c23200d2
Matheus Arthur <matheus.artsilva@gmail.com>
2026-03-06 18:05:29 -0300

    learn(ap-04): encerra a apostila

--- ap04-props/src/old_App.jsx
+++ ap04-props/src/old_App.jsx
@@ -1,24 +1,38 @@
 const App = () => (
   <div className="container border py-5">
+    
     <div className="row">
-      <div className="col-sm-12 col-md-3 col-lg-3 p-4 bg-primary">
-        Caixa vermelha
+      <div className="col-sm-12 col-md-3 col-lg-3">
+        <div className="p-4 bg-primary">
+
+        </div>
+        
       </div>
-      <div className="col-sm-12 col-md-9 col-lg-9 p-4 bg-secondary">
-        Caixa azul
+      <div className="col-sm-12 col-md-9 col-lg-9">
+        <div className="p-4 bg-primary">
+
+        </div>
+      </div>
+
+    </div>
+
+    <div className="row">
+      {/* .col-sm-12.col-md-6.col-lg-4.bg-info.p-4*3 */}
+      <div className="col-sm-12 col-md-6 col-lg-4">
+        <div className="bg-info p-4 border"></div>
+      </div>
+      <div className="col-sm-12 col-md-6 col-lg-4">
+        <div className="bg-info p-4 border"></div>
+      </div>
+      <div className="col-sm-12 col-md-12 col-lg-4">
+        <div className="bg-info p-4 border"></div>
       </div>
     </div>
 
     <div className="row">
-      <div className="col-sm-12 col-md-6 col-lg-4 p-4 bg-primary"></div>
-      <div className="col-sm-12 col-md-6 col-lg-4 p-4 bg-secondary"></div>
-      <div className="col-sm-12 col-md-12 col-lg-4 p-4 bg-primary"></div>
-
-    <div className="row">
-      <col-sm-12 className="col-md-6 col-lg-4 bg-info p-4"></col-sm-12>
-      <col-sm-12 className="col-md-6 col-lg-4 bg-info p-4"></col-sm-12>
-      <col-sm-12 className="col-md-6 col-lg-4 bg-info p-4"></col-sm-12>
-    </div>
+      <div className="col-12">
+        <div className="bg-success p-4"></div>
+      </div>
     </div>
   </div>
 )
