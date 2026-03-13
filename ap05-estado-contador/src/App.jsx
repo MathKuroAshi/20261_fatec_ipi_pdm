1183e6ba5ecc0e65f8ac690072043e34d5d3fd2d
Matheus Arthur <matheus.artsilva@gmail.com>
2026-03-13 18:15:00 -0300

    learn(ap05): lida com a pag 14
    - muda o nome de "primeiro-app-react-jsx" para "ap03-primeiro-app-react-jsx"

---
+++ ap05-estado-contador/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [contador, setContador] = useState(0)
+  return (
+    <div style={{textAlign: 'center', marginTop: 50}}>
+      <h1>
+        Meu contador: {contador}
+      </h1>
+      <button
+      onClick={() => setContador(contador + 1)}
+      style={{padding: 4}}>
+        Incrementar
+      </button>
+      <button
+      onClick={() => setContador(contador - 1)}
+      style={{padding: 4}}>
+        Decrementar
+      </button>
+      <button
+      onClick={() => setContador(0)}
+      style={{padding: 4}}>
+        Reiniciar
+      </button>
+    </div>
+  )
+}
+
+export default App
